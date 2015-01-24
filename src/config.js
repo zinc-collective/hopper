e583c0e4aedc3b97b1e852f7568da79568f3ccea
Zee Spencer <zee@zeespencer.com>
2015-01-23 16:49:56 -0800

    Loading non-existant config file returns an empty configuration hash

--- src/config.js
+++ src/config.js
@@ -9,6 +9,9 @@ var DEFAULT_CONFIG_FILE = expandHomeDir('~/.hop/config');
 
 // Loads configuration from path, parses JSON and returns POJO
 function fromFile(configFilePath) {
+  if (!fs.existsSync(configFilePath)) {
+    return {};
+  }
   var contents = fs.readFileSync(configFilePath);
   return JSON.parse(contents);
 }
--- test/config_tests.js
+++ test/config_tests.js
@@ -13,6 +13,11 @@ config.workspaces[workspaceB] = { depth: 2 };
 var configFilePath = path.resolve('test/fixtures/boring-config');
 
 c.toFile(configFilePath, config);
-
 var loadedConfig = c.fromFile(configFilePath);
 assert.deepEqual(loadedConfig, config);
+
+var configFilePath = path.resolve('test/fixtures/nonexistant-config');
+var loadedConfig = c.fromFile(configFilePath);
+assert.deepEqual(loadedConfig, {});
+
+console.log("OK!");
