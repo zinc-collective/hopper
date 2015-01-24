abeafb3a3e04678a9b17721395a74915721616e5
Zee Spencer <zee@zeespencer.com>
2015-01-23 20:41:25 -0800

    User may add a workspace with hop workspaces add

--- src/config.js
+++ src/config.js
@@ -10,15 +10,20 @@ var DEFAULT_CONFIG_FILE = expandHomeDir('~/.hop/config');
 
 // Loads configuration from path, parses JSON and returns POJO
 function fromFile(configFilePath) {
+  configFilePath = configFilePath || DEFAULT_CONFIG_FILE;
+
   if (!fs.existsSync(configFilePath)) {
     return {};
   }
+
   var contents = fs.readFileSync(configFilePath);
+
   return JSON.parse(contents);
 }
 
 // Stringifies POJO to JSON and overwrites the config file
-function toFile(configFilePath, config) {
+function toFile(config, configFilePath) {
+  configFilePath = configFilePath || DEFAULT_CONFIG_FILE;
   if (!fs.existsSync(configFilePath)) {
     fs.mkdirSync(path.dirname(configFilePath));
   }
--- test/config_tests.js
+++ test/config_tests.js
@@ -4,6 +4,7 @@ var c      = require('../src/config')
 ,   assert = require('assert');
 
 
+// Test setup, with them there fixtures
 var workspaceA = path.resolve('test/fixtures/workspace-a');
 var workspaceB = path.resolve('test/fixtures/workspace-b');
 
@@ -11,21 +12,25 @@ var config = { workspaces: { } };
 config.workspaces[workspaceA] = { depth: 1 };
 config.workspaces[workspaceB] = { depth: 2 };
 
-var configFilePath = path.resolve('test/fixtures/boring-config');
 
-c.toFile(configFilePath, config);
+
+// Writing and loading seems to work
+var configFilePath = path.resolve('test/fixtures/boring-config');
+c.toFile(config, configFilePath);
+
 var loadedConfig = c.fromFile(configFilePath);
 assert.deepEqual(loadedConfig, config);
 
+
+// Loads an empty object if the config file doesn't exist
 var configFilePath = path.resolve('test/fixtures/nonexistant-config');
 var loadedConfig = c.fromFile(configFilePath);
 assert.deepEqual(loadedConfig, {});
 
-
-
+// Create config and config directory if it doesn't exist
 ensureNonexistantConfigDoesntExist();
 var configFilePath = path.resolve('test/fixtures/nonexistant-directory/nonexistant-config');
-c.toFile(configFilePath, {});
+c.toFile({}, configFilePath);
 ensureNonexistantConfigDoesntExist();
 
 console.log("OK!");
