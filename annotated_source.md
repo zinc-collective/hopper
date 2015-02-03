f5307daba7343a3136372bca2a9bf3ad27ce87d0
@zip <->
2015-02-03 14:48:20 -0800

    Added more comments and rebuilt documentation

--- annotated_source.md
+++ annotated_source.md
@@ -2,3 +2,4 @@
 Back [home](./index.md)
  * [src/config.js](./src/config.html)
  * [src/projects.js](./src/projects.html)
+ * [src/workspaces.js](./src/workspaces.html)
--- src/workspaces.js
+++ src/workspaces.js
@@ -3,17 +3,24 @@ var expandHome = require('expand-home-dir');
 var path = require('path');
 var fs = require('fs');
 
-function addPaths(workspaces, paths, depth, err) {
+// Augment a `workspaces` object with new `paths`
+// workspaces: a workspace object
+// paths: an array of paths to add to the workspaces
+// depth: the depth parameter for each supplied workspace. Default: 1
+// errHandler: A handler which is invoked if a directory does not exist
+function addPaths(workspaces, paths, depth, errHandler) {
   if (depth === undefined) {
     depth = 1;
   }
 
-  if (err === undefined) {
-    err = function (path) {
+  // Install a default handler which simply warns on the console
+  if (errHandler === undefined) {
+    errHandler = function (path) {
       console.warn("Could not find path:", path);
     };
   }
 
+  // Poor man's type check
   assert(typeof workspaces === "object", "workspaces must be an object");
   assert(paths instanceof Array, "paths must be an array");
   assert(typeof depth === "number", "depth must be an integer or undefined");
@@ -21,10 +28,12 @@ function addPaths(workspaces, paths, depth, err) {
   // Append each argument as a path
   paths.forEach(function(workspacePath) {
     var expandedPath = path.resolve(expandHome(workspacePath));
+    // Check that the path exists
     if (fs.existsSync(expandedPath)) {
       workspaces[expandedPath] = { depth: depth };
     } else {
-      err(expandedPath);
+      // Invoke an error handler if the path is not found.
+      errHandler(expandedPath);
     }
   });
 
